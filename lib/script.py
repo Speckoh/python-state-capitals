ee3886d6b609ba1c0ab285643537d48b0eda7ddc
speckoh <speckohart@gmail.com>
2023-03-06 10:19:58 -0500

    states and caps

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,21 @@
+import random
 from capitals import states
 
-print(states)
+player_input = ''
+correct_score = 0
+num_of_questions = 10
+
+random.shuffle(states)
+
+print('Welcome! Name of the capital of each State!')
+
+for state in states[0:num_of_questions]:
+    print(state['name'])
+    player_input = input(f'What is the capital of {state["name"]}? ').lower()
+    if player_input == state["capital"].lower():
+        print("correct!")
+        correct_score += 1
+    else:
+        print("wrong!")
+
+print(f'You got {correct_score} / {num_of_questions} correct!')
